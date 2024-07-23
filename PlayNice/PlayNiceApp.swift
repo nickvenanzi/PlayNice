48df5a604254323b80fed63f700a4f326e446147
Nick Venanzi <nickvenanzi@gmail.com>
2024-07-23 18:31:55 -0400

    Initial Commit

---
+++ PlayNice/PlayNiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayNiceApp.swift
+//  PlayNice
+//
+//  Created by Nick Venanzi on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayNiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
